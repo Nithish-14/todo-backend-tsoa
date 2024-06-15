72abc2f6e529422c912a3bf5fe1dfe8e6578ab53
Nithish <nithishm612345@gmail.com>
2024-06-16 00:25:37 +0530

    generated node client

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@
 import express, {json, urlencoded} from "express";
 import { RegisterRoutes } from "../build/routes";
 
+import { Response as ExResponse, Request as ExRequest } from "express";
+import swaggerUi from "swagger-ui-express";
+
 export const app = express();
 
 // Use body parser to read sent json payloads
@@ -10,6 +13,12 @@ export const app = express();
 //     extended: true,
 //   })
 // );
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+    return res.send(
+      swaggerUi.generateHTML(await import("../build/swagger.json"))
+    );
+});
 
 
 app.use(json());
